9e7e270c7c511410327bb9398a4f3f3ba90db77a
KIM NAN WOO <49811923+gotree94@users.noreply.github.com>
2026-03-25 19:38:39 +0900

    Delete ai_iot directory

--- ai_iot/9_1_1.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import torch
-import ultralytics
-import numpy
-
-print("cv2:", cv2.__version__)
-print("torch:", torch.__version__)
-print("ultralytics:", ultralytics.__version__)
-print("numpy:", numpy.__version__)
